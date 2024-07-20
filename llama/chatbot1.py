278bea4825830e06642973e099c844da526b00b4
Yiğit Tilaver <yigittilaver2000@gmail.com>
2024-07-20 23:52:39 +0200

    created chatbot1

---
+++ llama/chatbot1.py
@@ -0,0 +1,25 @@
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = "r8_GghSa2teXj7CSPaU3ltIiRsI9onq6sw491nK2"
+
+import replicate
+
+# Prompts
+pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+prompt_input = input("How Can assist you today?: ")
+
+# Generate LLM response
+output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # LLM model
+                        input={"prompt": f"{pre_prompt} {prompt_input} Assistant: ", # Prompts
+                        "temperature":0.1, "top_p":0.9, "max_length":128, "repetition_penalty":1})  # Model parameters
+
+
+# print(output)
+
+full_response = ""
+
+for item in output:
+  full_response += item
+
+print(full_response)
+
